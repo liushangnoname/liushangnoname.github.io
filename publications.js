e926f16c61bea81d4f1ae9163cc19f365d6e5dbb
Shang Liu <liushangnoname@gmail.com>
2026-08-27 03:11:58 +0100

    Reorganized website contents

--- publications.js
+++ publications.js
@@ -12,7 +12,7 @@
       return match ? parseInt(match[1].replace('.', ''), 10) : 0;
     }
 
-    function wrapTldr(paper) {
+    function prepareTldr(paper, paperIndex) {
       var italicText = paper.querySelectorAll('i');
       var tldrLabel = null;
 
@@ -29,19 +29,41 @@
 
       var paragraph = tldrLabel.parentNode;
       var wrapper = document.createElement('span');
+      var toggle = document.createElement('button');
+      var paperView = paper.closest('.publication-view');
+      var tldrId = paperView.id + '-tldr-' + paperIndex;
+      var arxivLink = paper.querySelector('a[href*="arxiv.org/abs/"]');
+      var insertionPoint = arxivLink && arxivLink.nextSibling;
+      var breakBeforeTldr = tldrLabel.previousElementSibling;
       var node = tldrLabel;
+
+      if (breakBeforeTldr && breakBeforeTldr.tagName === 'BR') {
+        paragraph.removeChild(breakBeforeTldr);
+      }
+
       paragraph.insertBefore(wrapper, tldrLabel);
       wrapper.className = 'paper-tldr';
+      wrapper.id = tldrId;
+      wrapper.hidden = true;
 
       while (node) {
         var nextNode = node.nextSibling;
         wrapper.appendChild(node);
         node = nextNode;
       }
-    }
 
-    for (var topicIndex = 0; topicIndex < topicPapers.length; topicIndex += 1) {
-      wrapTldr(topicPapers[topicIndex]);
+      toggle.type = 'button';
+      toggle.className = 'paper-tldr-toggle';
+      toggle.textContent = 'TL;DR ▾';
+      toggle.setAttribute('aria-controls', tldrId);
+      toggle.setAttribute('aria-expanded', 'false');
+      toggle.setAttribute('aria-label', 'Show TL;DR');
+
+      if (insertionPoint) {
+        paragraph.insertBefore(toggle, insertionPoint.nextSibling);
+      } else {
+        paragraph.insertBefore(toggle, wrapper);
+      }
     }
 
     var chronologicalPapers = Array.prototype.slice.call(topicPapers);
@@ -51,6 +73,24 @@
 
     for (var paperIndex = 0; paperIndex < chronologicalPapers.length; paperIndex += 1) {
       chronologicalList.appendChild(chronologicalPapers[paperIndex].cloneNode(true));
+    }
+
+    var allPapers = document.querySelectorAll('.publication-view ol > li');
+    for (var allPaperIndex = 0; allPaperIndex < allPapers.length; allPaperIndex += 1) {
+      prepareTldr(allPapers[allPaperIndex], allPaperIndex);
+    }
+
+    var tldrToggles = document.querySelectorAll('.paper-tldr-toggle');
+    for (var toggleIndex = 0; toggleIndex < tldrToggles.length; toggleIndex += 1) {
+      tldrToggles[toggleIndex].addEventListener('click', function () {
+        var isExpanded = this.getAttribute('aria-expanded') === 'true';
+        var tldr = document.getElementById(this.getAttribute('aria-controls'));
+
+        tldr.hidden = isExpanded;
+        this.setAttribute('aria-expanded', isExpanded ? 'false' : 'true');
+        this.setAttribute('aria-label', isExpanded ? 'Show TL;DR' : 'Hide TL;DR');
+        this.textContent = isExpanded ? 'TL;DR ▾' : 'TL;DR ▴';
+      });
     }
 
     function showView(viewName) {
